0ea52f0e1e37742ec7d43d785c45969862b1893f
Casey Calouette <casey.calouette@gmail.com>
2024-08-30 09:30:35 -0400

    Cleaned up Basic Linear.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/BasicLinearFieldCent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/BasicLinearFieldCent.java
@@ -221,28 +221,6 @@ public class BasicLinearFieldCent extends LinearOpMode {
 
 
 
-/*
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
-            }
-
-*/
 
 
             // Send calculated power to wheels
